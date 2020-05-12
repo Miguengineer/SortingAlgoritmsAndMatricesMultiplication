d619c8db3132f73ab1966d30a8c82daa2a1958a6
krampus <durandiazmiguel@gmai.com>
2020-05-11 22:48:00 -0400

    Almost done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <valarray>
+#include <chrono>
 #include "sorting_algorithms.h"
 #include "matrices_mult.h"
 #include "utils.h"
+typedef long long lld;
 /*
  * Documentation...
  * ...
@@ -10,60 +12,15 @@
  */
 using namespace std;
 
-void Initialise(int **res, int rows, int cols)
-{
-    for (int i = 0; i < rows; i++)
-    {
-        for(int j = 0; j < cols; j++)
-        {
-            res[i][j] = i*j; // whatever you want.
-        }
-    }
-}
 
-int main() {
-    /*
-    int aRows = 3;
-    int aCols = 2;
-    int bRows = 2;
-    int bCols = 4;
-    // Array of pointers
-    int** A = new int*[aRows];
-    for (int i = 0; i < aRows; i++)
-    {
-        A[i] = new int[aCols];
-    }
 
-    int** B = new int*[bRows];
-    for (int i = 0; i < bRows; i++)
-    {
-        B[i] = new int[bCols];
-    }
+    int main() {
 
-    Initialise(A, aRows, aCols);
-    Initialise(B, bRows, bCols);
-
-    int** C;
-    C = new int*[aRows];
-    for (int i = 0; i < aRows; i++)
-    {
-        C[i] = new int [bCols];
-    }
-
-    mult_with_space_locality(C,A,B,aRows,aCols,bRows,bCols);
-
-    for (int i = 0; i < aRows; i++)
-    {
-        for (int j = 0; j < bCols; j++)
-        {
-            std::cout << C[i][j] << ' ';
-        }
-        std::cout << '\n';
-    }
-    */
-    test_std_sort();
-
-    return 0;
+        test_std_sort();
+        test_quicksort();
+        test_selectionsort();
+        test_bubblesort();
+        return 0;
 
 
 
